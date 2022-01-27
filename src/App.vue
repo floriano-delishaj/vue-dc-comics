5a58a769245e248b8053fbee3316a5ac6f965194
floriano-delishaj <delishaj.floriano@live.it>
2022-01-27 13:30:20 +0100

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <header-content />
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import HeaderContent from './components/HeaderContent.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderContent,
+    }
   }
-}
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import '@/style/main.scss';
+
 </style>
